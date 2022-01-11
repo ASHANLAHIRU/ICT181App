b9112e32531c1d13abcc1e4d2233fc2168b8a101
ASHAN <ashanlahiru1998@gmail.com>
2022-01-11 16:36:20 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+class StudentParent{
+            	public $stdName;
+            	public $stdRegNum;
+
+            	function __construct($name, $regnum) {
+            		$this->stdName = $name;
+            		$this->stdRegNum = $regnum;
+			  	}
+
+			  	
+            }
